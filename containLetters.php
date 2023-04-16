75b51447439ac2cbbd0c3b5b1ea19b46b1bea268
zakuurr <ezakurnia50@gmail.com>
2023-04-16 22:29:32 +0700

    first commit

--- containLetters.php
+++ containLetters.php
@@ -1,23 +1,21 @@
 <?php
+function containLetters($firstWord, $secondWord){
+    $total_found = 0;
+    $length = strlen($firstWord);
+    for ($x = 0; $x < $length; $x++) {
+        $chr = substr($firstWord, $x, 1);            
+        if(strpos(strtolower($secondWord), $chr) !== false){
+            $total_found = $total_found + 1;
+        }  
+    }
 
-function containLetters($firstWord, $secondWord)
-{
-    $chr = strtolower($firstWord);
-    $second = strtolower($secondWord);
-
-    $length = str_split($chr);
     $status = false;
-
-    foreach ($length as $huruf) {
-        if (str_contains($second, $huruf)) {
-            $status = true;
-        } else {
-            $status = false;
-            break;
-        }
+    if($total_found < 3){
+        $status = false;
+    }else{
+        $status = true;
     }
     return $status;
-
 }
 
 $boolean = null;
@@ -41,21 +39,15 @@ if (isset($_POST['first_word']) && isset($_POST['second_word'])) {
 
 <!doctype html>
 <html lang="en">
-
-<head>
-    <!-- Required meta tags -->
+  <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Reza Kurnia - Task 1</title>
-</head>
-
-<body>
-    <div class="container mt-2">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  </head>
+  <body>
+  <div class="container mt-2">
+  <h1>Contain Letters</h1>
         <form action="" method="POST">
             <div class="row">
             <div class="card" style="width: 100%;">
@@ -74,14 +66,20 @@ if (isset($_POST['first_word']) && isset($_POST['second_word'])) {
                 </div>
               
                 <div class="col-md-12">
-                    <button type="submit" class="btn btn-success" id="btn-check">Cek</button>
+                    <button type="submit" class="btn btn-success mt-3">Cek</button>
                
                     <?php if($boolean != null): ?>
             <div class="row mt-2">
                 <div class="col-md-12">
-                    <div class="alert alert-success" role="alert">
-                        <?php echo $boolean; ?>
+                    <?php if($boolean == "True") : ?>
+                <div class="alert alert-success" role="alert">
+                     <?php echo $boolean; ?>
                     </div>
+                    <?php else :?>
+                        <div class="alert alert-danger" role="alert">
+                     <?php echo $boolean; ?>
+                    </div>
+                    <?php endif; ?>
                 </div>
             <?php endif; ?>
                 </div>
@@ -95,12 +93,7 @@ if (isset($_POST['first_word']) && isset($_POST['second_word'])) {
            
         </form>
     </div>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-    </script>
-</body>
-
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  </body>
 </html>
